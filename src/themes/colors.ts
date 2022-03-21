cf06aef756ecdf2b6306d599d67cb14c463c1f54
Bruno Racobaldo <bruno@startapp.one>
2022-03-21 10:15:38 -0300

    finalized themes

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -11,7 +11,21 @@ const yellow = {
 	900: "#00191e",
 };
 
+const black = {
+	50: "#F2F2F2",
+	100: "#DBDBDB",
+	200: "#C4C4C4",
+	300: "#ADADAD",
+	400: "#969696",
+	500: "#808080",
+	600: "#666666",
+	700: "#4D4D4D",
+	800: "#333333",
+	900: "#000000",
+};
+
 export const colors = {
 	yellow,
-	primary: yellow,
+	black,
+	primary: black,
 };
